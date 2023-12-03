a0fce32886ab0524124149ec95bb568965d76bfd
Nadira3 <paitanun35@gmail.com>
2023-12-03 12:51:55 +0100

    (feat) => package file containing first implementation of database management using json format

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module contains storage file initialization """
+
+from models import base_model, history, complaint, state, patient, review, utils
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
